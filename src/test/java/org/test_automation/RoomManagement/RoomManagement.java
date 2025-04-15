e168bb611a6d9cabc7e939876a70c89b4d142b8d
sharma-39 <55731879+sharma-39@users.noreply.github.com>
2025-04-15 15:25:25 +0530

    changes

--- src/test/java/org/test_automation/RoomManagement/RoomManagement.java
+++ src/test/java/org/test_automation/RoomManagement/RoomManagement.java
@@ -6,15 +6,131 @@ import org.test_automation.DBConnectivity.XPathUtil;
 import org.test_automation.LoginUtil.LoginAndLocationTest;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class RoomManagement extends LoginAndLocationTest {
 
-
     private final XPathUtil xPathUtil = new XPathUtil();
+    List<String> roomTypes = Arrays.asList(
+            "General Ward",
+            "Private Room",
+            "Semi-Private Room",
+            "ICU (Intensive Care Unit)",
+            "NICU (Neonatal Intensive Care Unit)",
+            "PICU (Pediatric Intensive Care Unit)",
+            "Emergency Room",
+            "Operation Theater",
+            "Post-Operative Room",
+            "Recovery Room (PACU)",
+            "Isolation Room",
+            "Quarantine Room",
+            "Labor Room",
+            "Delivery Room",
+            "Maternity Ward",
+            "Consultation Room",
+            "Examination Room",
+            "Procedure Room",
+            "Radiology Room",
+            "Dialysis Room",
+            "Chemotherapy Room",
+            "Burn Unit",
+            "Physiotherapy Room",
+            "Pediatric Ward",
+            "Surgical Ward",
+            "Orthopedic Ward",
+            "Cardiology Ward",
+            "Neurology Ward",
+            "Oncology Ward",
+            "Gastroenterology Room",
+            "Urology Room",
+            "ENT Room",
+            "Dermatology Room",
+            "Ophthalmology Room",
+            "Dental Room",
+            "Psychiatry Room",
+            "Counseling Room",
+            "Triage Room",
+            "Injection Room",
+            "Minor OT",
+            "Medical Storage Room",
+            "Vaccination Room",
+            "Observation Room",
+            "Telemedicine Room",
+            "Medical Imaging Room",
+            "Catheterization Lab (Cath Lab)",
+            "Endoscopy Room",
+            "Blood Collection Room",
+            "Staff Rest Room",
+            "Medical Records Room"
+    );
+    List<String> wardNames = Arrays.asList(
+            "General Ward",
+            "Male Ward",
+            "Female Ward",
+            "Pediatric Ward",
+            "Surgical Ward",
+            "Medical Ward",
+            "ICU Ward",
+            "NICU Ward",
+            "PICU Ward",
+            "Maternity Ward",
+            "Postnatal Ward",
+            "Antenatal Ward",
+            "Orthopedic Ward",
+            "Cardiology Ward",
+            "Neurology Ward",
+            "Oncology Ward",
+            "Burn Ward",
+            "Isolation Ward",
+            "Infectious Disease Ward",
+            "Psychiatric Ward",
+            "Rehabilitation Ward",
+            "ENT Ward",
+            "Urology Ward",
+            "Gastroenterology Ward",
+            "High Dependency Unit (HDU)",
+            "Emergency Ward",
+            "Dialysis Ward",
+            "Geriatric Ward",
+            "Private Ward",
+            "Day Care Ward"
+    );
+    List<String> floors = Arrays.asList(
+            "Ground Floor",
+            "First Floor",
+            "Second Floor",
+            "Third Floor",
+            "Basement",
+            "Mezzanine Floor",
+            "Lower Ground Floor",
+            "Service Floor",
+            "Emergency Floor",
+            "Operation Theater Floor",
+            "ICU Floor",
+            "Diagnostic Floor",
+            "Maternity Floor",
+            "Outpatient Floor (OPD)",
+            "Inpatient Floor (IPD)",
+            "Administrative Floor",
+            "Pharmacy Floor",
+            "Lab Floor",
+            "Radiology Floor",
+            "Rehabilitation Floor",
+            "Specialty Floor",
+            "Consultation Floor",
+            "Executive Floor",
+            "Cafeteria Floor",
+            "Parking Basement"
+    );
     String floorName;
     String wardName;
     String roomType;
+
+
+    Random random = new Random();
+    String randomRoomType = roomTypes.get(random.nextInt(roomTypes.size()));
 
     String roomNo;
 
@@ -29,7 +145,7 @@ public class RoomManagement extends LoginAndLocationTest {
         clickButtonElement(By.xpath("//button[contains(text(),'Add')]"));
 
 
-        floorName = generateRondamNumber("FLOOR");
+        floorName = floors.get((int) (Math.random() * roomTypes.size()));
         xPathUtil.fillTextField("floorName", floorName, wait);
 
         String description = "desc";
@@ -41,7 +157,7 @@ public class RoomManagement extends LoginAndLocationTest {
         clickButtonElement(By.xpath("//button[contains(text(),'Add')]"));
 
 
-        wardName = generateRondamNumber("WARD");
+        wardName = wardNames.get((int) (Math.random() * roomTypes.size()));
 
         xPathUtil.fillTextField("wardName", wardName, wait);
 
@@ -56,8 +172,7 @@ public class RoomManagement extends LoginAndLocationTest {
         clickButtonElement(By.xpath("//a[@id='Room Type' and contains(@class, 'nav-link')]"));
         clickButtonElement(By.xpath("//button[contains(text(),'Add')]"));
 
-        roomType = generateRondamNumber("RT");
-
+        roomType = roomTypes.get((int) (Math.random() * roomTypes.size()));
         xPathUtil.fillTextField("roomTypeName", roomType, wait);
 
         String uomId = "TEST-UOM";
